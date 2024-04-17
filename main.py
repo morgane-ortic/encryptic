6e0a93040d4aebfb5d9d74623bdb78d9990a0407
Morgane Ortic <morgane.ortic@proton.me>
2024-04-17 10:18:09 +0200

    merged the cool login interface and encryption functionality into main.py :)

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import time
 import os
+from cryptography.fernet import Fernet
+from stringcolor import cs
 
 clear = lambda: os.system('clear')
 clear()
 
 #Global variables
+decrypted = False
 username = ""
 password = ""
+message = ""
 credentials = []
 
 # Functions
 def register():
-    username = input("Enter a new username: ")
-    password = input("Enter a new password: ")
+    global username, password, encMessage, fernet
+
+    username = input(cs("Enter your name: ", "cyan"))
+    password = input(cs("Enter a password: ", "cyan"))
+    message = input(f"Hi {cs(username.title(), 'cyan')}, enter the message you want to encrypt: ")
+    
+# generate a key for encryption and decryption
+# You can use fernet to generate 
+# the key or use random key generator
+# here using fernet to generate key
+    key = Fernet.generate_key()
+# Instance the Fernet class with the key
+    fernet = Fernet(key)
+# then use the Fernet class instance 
+# to encrypt the string string must
+# be encoded to byte string before encryption
+    encMessage = fernet.encrypt(message.encode())
     credentials.append(username)
     credentials.append(password)
     clear()
@@ -37,22 +56,33 @@ def register():
     clear()
     login()
 
+# decrypt the encrypted string with the 
+# Fernet instance of the key,
+# that was used for encrypting the string
+# encoded byte string is returned by decrypt method,
+# so decode it to string with decode methods
+def decryption():
+    decMessage = fernet.decrypt(encMessage).decode()
+    print("decrypted string: ", decMessage)
+
 def login():
-    username_login = input("Enter your login username: ")
-    password_login = input("Enter your password: ")
-    clear()
-    if username_login == credentials[0] and password_login == credentials[1]:
-        clear()
-        print("Login successful!")
-        time.sleep(2)
-        print("\nWelcome, " + username_login + "!")  # Example of concatenation
-        time.sleep(2)
-        clear()
-    else:
-        print("Login failed. Please try again.")
-        time.sleep(2)
-        clear()
-        login()
+    global decrypted
+    while decrypted == False:
+        name_input = input("What\'s your name? ")
+        if name_input == username and decrypted == False:
+            while True:  # added this line
+                time.sleep(2)
+                password_input = input(f"Hello, {username}. Please enter your password: ")
+                name_given = True
+                if password_input == password:
+                    time.sleep(2)
+                    decrypted = True
+                    decryption()
+                    break  # added this line
+                else:
+                    time.sleep(2)
+                    print("This is not the password >:()")
+
 
 
 #======================================================================================================
