6dd3b5dcc375e6eb72ff3b150855dc712c4e0324
whatmeansgithub <drinkyoghurt@gmail.com>
2024-04-24 11:50:03 +0200

    changed yes/no confirmation

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def register_account():     # Function that lets you register a new account
             print('Yes or no? ')                                          #
             time.sleep(1)                                                 #
             clear()                                                       #
-        if initial_message == 'yes':                                      # if the user enters 'yes' - the program will allow the user to enter a message
+        if initial_message.startswith('y'):                                      # if the user enters 'yes' - the program will allow the user to enter a message
             clear()                                                       #
             messages = input(f"Hi {cs(usernames.title(), 'cyan')}, enter the message you want to encrypt: ") # ask the user to enter a message
             adding_date_to_message()                                      # add the date and time to the message
@@ -106,7 +106,7 @@ def register_account():     # Function that lets you register a new account
             print(print_letters_appart(encMessages))                      # print the dots separately to make the program look like it's encrypting the message and loading
             print(cs("\nMessage added!" , "yellow"))                      #
             break                                                         # break the loop when the user enters a valid message
-        elif initial_message == 'no':                                     # if the user enters 'no' - the program will not allow the user to enter a message
+        elif initial_message.startswith('n'):                                     # if the user enters 'no' - the program will not allow the user to enter a message
             clear()                                                       #
             break                                                         # break the loop when the user enters 'no'
         elif initial_message != 'yes' or initial_message != 'no':         # if the user enters something other than 'yes' or 'no'
