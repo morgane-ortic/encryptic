6a835d9d53b6daf5c70fd65297686c1ac4cf5276
whatmeansgithub <drinkyoghurt@gmail.com>
2024-04-26 10:49:44 +0200

    roger please help with display messages

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ def register_account():     # Function that lets you register a new account
             print(cs("\nMessage added!" , "yellow"))                      #   
             time.sleep(2)                                                 #      
             break                                                         # break the loop when the user enters a valid message
-        elif initial_message.startswith('n'):                                     # if the user enters 'no' - the program will not allow the user to enter a message
+        elif initial_message.startswith("n"):                                     # if the user enters 'no' - the program will not allow the user to enter a message
             clear()                                                       #
             break                                                         # break the loop when the user enters 'no'
         elif initial_message != 'yes' or initial_message != 'no':         # if the user enters something other than 'yes' or 'no'
